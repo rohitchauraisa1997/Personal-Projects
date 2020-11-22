bec59c1e707921547cf2a0e8fd26ef2589f004ba
rohit_1997 <rohit.chaurasia997@gmail.com>
2020-11-22 23:59:45 +0900

    deleting irrelevant things

--- sudoku_solver/build.py
+++ sudoku_solver/build.py
@@ -123,12 +123,9 @@ if __name__ == "__main__":
     print_sudoku_board(sample_board)
     execute(sample_board)
     print("++++++++++++++++++++++++++\n\n\n\n\n\n")
-    print("22222222222222")
     print_sudoku_board(sample_board)
     for i in range(10):
         for j in range(10):
             print((i,j),end = " ")
             if j == 9:
                 print("\n")
-                print("new line")
-                print("again")
