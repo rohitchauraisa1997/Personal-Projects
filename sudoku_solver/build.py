465686a5352419cc2bc74cab4dffec75f7fe66b5
rohit_1997 <rohit.chaurasia997@gmail.com>
2020-11-22 22:51:34 +0900

    editing build.py

--- sudoku_solver/build.py
+++ sudoku_solver/build.py
@@ -23,6 +23,7 @@ def validate_entry(board, num, pos):
             return False
     
     # checking whether same 3*3 box doesnt have duplicate entry
+    
     '''
     (0, 0) (0, 1) (0, 2) |  (0, 3) (0, 4) (0, 5) |  (0, 6) (0, 7) (0, 8) 
 
