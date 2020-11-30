1e1db269a9a6e9bf2477d2709320f3eafd46ffb0
rohit_1997 <rohit.chaurasia997@gmail.com>
2020-11-30 22:29:29 +0900

    adding move and jump to enable bird movement

--- flappy bird/flappy.py
+++ flappy bird/flappy.py
@@ -30,14 +30,21 @@ BG_IMG = pygame.transform.scale2x(
 
 class Bird:
     IMGS = BIRD_IMGS
+    # how much bird tilts while moving up and down
     MAX_ROTATION = 25
+    # how much we rotate on each frame 
     ROT_VEL = 20
     ANIMATION_TIME = 5
 
     def __init__(self, x, y):
+        # starting pos of our bird
         self.x = x
         self.y = y
+        # how much the image is tilted initialised
+        # by 0 since its flat in the beginning.
         self.tilt = 0
+        # defines the physics related to that 
+        # particular bird 
         self.tick_count = 0
         self.vel = 0
         self.height = self.y
@@ -45,9 +52,36 @@ class Bird:
         self.img = self.IMGS[0]
     
     def jump(self):
-        self.vel = 10.5
+        #called when we need
+        #the bird to flap up
+        #-ve velocity enables us to jump up
+        #(0,0) is the top lhs cordinate
+        self.vel = -10.5
+        #keeps track of when we last jumped
+        #set tick_count = 0 for every jump
         self.tick_count = 0
+        #where the bird jumped from
         self.height = self.y
-    
+
     def move(self):
+        #move() enables the bird to move
         self.tick_count += 1 
+        # displacement tells us how much we move up or how much we move down
+        # tick_count tells us how much time we r moving for
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
+        #for tick_count = 1 ; -10.5*1 + 1.5*1**2 = -10.5 + 1.5 = -9 ----> moving 9 pixels upwards
+        #for tick_count = 2 ; -10.5*2 + 1.5*2**2 = -21 + 6 = -15 ----> moving 9 pixels upwards
+        
+        # if we r moving down more than 16 just move 16
+        if d>=16:
+            d = 16
+        
+        #fine tunes the movement
+        #d<0 implies we r moving upwards
+        if d<0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d<0 or self.y < slef.height + 50:
+            
