4e5281171491d7d8a4bdb44a5db319cf9f0b4ee7
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2020-12-15 11:16:38 +0900

    rendering jump

--- learning_pygame/learn.py
+++ learning_pygame/learn.py
@@ -39,15 +39,26 @@ while run:
     if keys[pygame.K_RIGHT] and x < SCREEN_WIDTH-width-vel:
         x += vel
 
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
+    if not is_jump:
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
 
-    if keys[pygame.K_DOWN] and y < SCREEN_HEIGHT-height-vel:
-        y += vel
+        if keys[pygame.K_DOWN] and y < SCREEN_HEIGHT-height-vel:
+            y += vel
 
-    if keys[pygame.K_SPACE]:
-        is_jump = True
-
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -10:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            y -= (jump_count ** 2)*neg
+            jump_count -=1
+        else:
+            is_jump = False    
+            jump_count = 10
+                
     win.fill((0,0,0))
     # rect parameters -> surface, color, rect
     pygame.draw.rect(win,(255,0,0),(x,y,width,height))
