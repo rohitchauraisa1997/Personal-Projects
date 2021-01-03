f315cc233e7aecb962c8d1fc7f35073611415e6f
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-01-03 23:00:02 +0900

    working algorithm

--- A_star/astar.py
+++ A_star/astar.py
@@ -44,7 +44,7 @@ class Spot:
         self.width = width
         self.total_rows = total_rows
     
-    def get_post(self):
+    def get_pos(self):
         return self.row,self.col
 
     def is_closed(self):
@@ -70,6 +70,9 @@ class Spot:
     def make_open(self):
         self.color = GREEN
         
+    def make_closed(self):
+        self.color = RED
+        
     def make_barrier(self):
         self.color = BLACK
         
@@ -85,30 +88,82 @@ class Spot:
     def make_start(self):
         self.color = ORANGE
     
-	def update_neighbors(self, grid):
-		self.neighbors = []
-		if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # Down the row
-			self.neighbors.append(grid[self.row + 1][self.col])
+    def update_neighbors(self, grid):
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # Down the row
+            self.neighbors.append(grid[self.row + 1][self.col])
 
-		if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # Uo the Row
-			self.neighbors.append(grid[self.row - 1][self.col])
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # Uo the Row
+            self.neighbors.append(grid[self.row - 1][self.col])
 
-		if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # Right in Column
-			self.neighbors.append(grid[self.row][self.col + 1])
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # Right in Column
+            self.neighbors.append(grid[self.row][self.col + 1])
 
-		if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # Left in Column
-			self.neighbors.append(grid[self.row][self.col - 1])
-    
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # Left in Column
+            self.neighbors.append(grid[self.row][self.col - 1])
+
     def __lt__(self,other):
         # compares spot objects
         # lessthan
         return False
     
+
+def reconstruct_path(came_from, current, draw):
+	while current in came_from:
+		current = came_from[current]
+		current.make_path()
+		draw()
+
+def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = h(start.get_pos(), end.get_pos())
+
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw)
+			end.make_end()
+			return True
+
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+
+		draw()
+
+		if current != start:
+			current.make_closed()
+
+	return False
+
 def h(p1,p2):
     # manhattan distance
     x1,y1 = p1
     x2,y2 = p2
-    return abs(x1-x2)+abc(y1-y2)
+    return abs(x1-x2)+abs(y1-y2)
 
 def make_grid(rows,width):
     grid = []
@@ -166,11 +221,6 @@ def main(win, width):
             if event.type == pygame.QUIT:
                 run = False
                 
-            if started:
-                # prohibits user from doing anything 
-                # once the a star algo starts running.
-                continue
-            
             # Left Mouse Button
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
@@ -201,8 +251,18 @@ def main(win, width):
                     end = None
                 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started :
-                    pass
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                            
+                    algorithm(lambda:draw(win, grid, ROWS, width),grid,start,end)
+                    
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS,width)
+                    
     pygame.quit()
     
 if __name__ == "__main__":
